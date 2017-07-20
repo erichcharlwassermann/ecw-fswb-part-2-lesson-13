6cf463742a0c28a1c87725eb84a4c2168ccbc7f6
Erich Charl Wassermann <erichcharlwassermann@gmail.com>
2017-07-20 15:15:12 +0200

    Fixed Server

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -4,8 +4,8 @@
 
 import http.server
 import requests
-import os
 from urllib.parse import unquote, parse_qs
+import os
 
 memory = {}
 
@@ -98,7 +98,6 @@ class Shortener(http.server.BaseHTTPRequestHandler):
                 "Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-port = int(os.environ.get('PORT', 8000))   # Use PORT if it's there.
-server_address = ('', port)
-httpd = http.server.HTTPServer(server_address, Shortener)
-httpd.serve_forever()
+    server_address = ('', 8000)
+    httpd = http.server.HTTPServer(server_address, Shortener)
+    httpd.serve_forever()
